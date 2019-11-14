6ff80a09a505a1521afa81ae04b590156f0f5c54
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-13 21:28:06 -0400

    obteniendo items de los contenedores

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,11 @@
-floatNotificationError("Error");
+/*container places | items places*/
+let containerPlacesImages = document.getElementById("container__images");
+let allPlaces = containerPlacesImages.querySelectorAll(".placeImage");
+
+/*images | items images*/
+let containerImages = document.getElementById("container__places");
+let itemImages = containerImages.querySelectorAll(".itemImages");
+
+
+
+
