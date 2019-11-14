51661d6fe48e66792f924dded0fb158e4b7551e8
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-13 22:34:02 -0400

    agregado logica de arrastre + limitacion de arrastre

--- public/js/main.js
+++ public/js/main.js
@@ -8,4 +8,64 @@ let itemImages = containerImages.querySelectorAll(".itemImages");
 
 
 
+/*
+* verifying if exist element images
+* */
+if(containerImages && allPlaces && itemImages && containerPlacesImages){
 
+    for(let img = 0; img < itemImages.length; img++){
+
+
+
+
+        allPlaces[img].addEventListener("dragenter", function(e){
+            e.preventDefault();
+
+            //setter class
+            allPlaces[img].classList.add("background");
+            setTimeout(()=>{
+                allPlaces[img].classList.remove("background");
+            },1000);
+
+
+        },false);
+
+
+
+        allPlaces[img].addEventListener("dragover", function(e){
+            e.preventDefault();
+        },false);
+
+
+        allPlaces[img].addEventListener("dragend", function(e){
+            e.preventDefault();
+        },false);
+
+        //start event
+        itemImages[img].addEventListener("dragstart", function(e){
+
+            let dataHtml = "<img src="+ e.target.src +" >";
+            e.dataTransfer.setData("Text", dataHtml);
+
+        },false);
+
+        let counterDrop = 0;
+        allPlaces[img].addEventListener("drop", function(e){
+            counterDrop+=1;
+
+            if(counterDrop <= 1){
+                allPlaces[img].innerHTML = e.dataTransfer.getData("Text");
+            }
+            else{
+                floatNotificationError("Imposible reemplazar")
+            }
+
+        },false);
+
+
+
+        //    end for
+    }
+
+
+}
