49d3738ac9092f9b8776db943a0c01eebbc53c80
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-13 22:43:52 -0400

    refactorizando codigo A

--- public/js/main.js
+++ public/js/main.js
@@ -1,71 +1,87 @@
-/*container places | items places*/
-let containerPlacesImages = document.getElementById("container__images");
-let allPlaces = containerPlacesImages.querySelectorAll(".placeImage");
+(function(){
 
-/*images | items images*/
-let containerImages = document.getElementById("container__places");
-let itemImages = containerImages.querySelectorAll(".itemImages");
+    /*container places | items places*/
+    let containerPlacesImages = document.getElementById("container__images"),
+        allPlaces = containerPlacesImages.querySelectorAll(".placeImage");
 
-
-
-/*
-* verifying if exist element images
-* */
-if(containerImages && allPlaces && itemImages && containerPlacesImages){
-
-    for(let img = 0; img < itemImages.length; img++){
+    /*images | items images*/
+    let containerImages = document.getElementById("container__places"),
+        itemImages = containerImages.querySelectorAll(".itemImages");
 
 
+    /*
+    * verifying if exist element images
+    * */
+    if(containerImages && allPlaces && itemImages && containerPlacesImages){
+
+        //rotating images
+        for(let img = 0; img < itemImages.length; img++){
 
 
-        allPlaces[img].addEventListener("dragenter", function(e){
-            e.preventDefault();
+            allPlaces[img].addEventListener("dragenter", function(e){
+                e.preventDefault();
 
-            //setter class
-            allPlaces[img].classList.add("background");
-            setTimeout(()=>{
+                //setter class
+                allPlaces[img].classList.add("background");
+
+                setTimeout(()=>{
+                    allPlaces[img].classList.remove("background");
+                },1000);
+
+
+            },false);
+
+
+
+            allPlaces[img].addEventListener("dragover", function(e){
+                e.preventDefault();
+            },false);
+
+
+            allPlaces[img].addEventListener("dragend", function(e){
+                e.preventDefault();
+            },false);
+
+            /*
+            * verifying if item is go of place
+            * */
+            allPlaces[img].addEventListener("dragleave", function(e){
+
                 allPlaces[img].classList.remove("background");
-            },1000);
+
+            },false);
 
 
-        },false);
+            /*
+                start event
+             */
+            itemImages[img].addEventListener("dragstart", function(e){
 
+                let dataHtml = "<img src="+ e.target.src +" >";
+                e.dataTransfer.setData("Text", dataHtml);
 
+            },false);
 
-        allPlaces[img].addEventListener("dragover", function(e){
-            e.preventDefault();
-        },false);
+            let counterDrop = 0;
 
+            allPlaces[img].addEventListener("drop", function(e){
 
-        allPlaces[img].addEventListener("dragend", function(e){
-            e.preventDefault();
-        },false);
+                counterDrop+=1;
 
-        //start event
-        itemImages[img].addEventListener("dragstart", function(e){
+                if(counterDrop <= 1){
+                    allPlaces[img].innerHTML = e.dataTransfer.getData("Text");
+                }
+                else{
+                    floatNotificationError("Imposible reemplazar")
+                }
 
-            let dataHtml = "<img src="+ e.target.src +" >";
-            e.dataTransfer.setData("Text", dataHtml);
-
-        },false);
-
-        let counterDrop = 0;
-        allPlaces[img].addEventListener("drop", function(e){
-            counterDrop+=1;
-
-            if(counterDrop <= 1){
-                allPlaces[img].innerHTML = e.dataTransfer.getData("Text");
-            }
-            else{
-                floatNotificationError("Imposible reemplazar")
-            }
-
-        },false);
+            },false);
 
 
 
-        //    end for
+            //    end for
+        }
+
+    // endif
     }
-
-
-}
+})();
